7b6324a13fa4409f2e775cf424a3e9737508c3c5
AndreaOl <olandeseandrea@gmail.com>
2021-06-11 08:34:38 +0000

    Aggiunto client python

---
+++ python/client.py
@@ -0,0 +1,58 @@
+import socket
+import _thread
+import sys
+
+PORT = 8080
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except:
+    print("\n Socket creation error \n")
+    exit()
+
+stop = 0
+
+def client_handler():
+    global sock, stop
+    while(True):
+        string = input()
+        sock.sendall(bytes(string))
+        if(string == "quit"):
+            print("QUIT SIIIIII\n")
+            stop = 1
+            break
+        print("Message sent\n")
+
+
+def main(addr: str):
+    global sock, stop
+
+    try:
+        socket.inet_pton(socket.AF_INET, addr)
+    except:
+        print("\nInvalid address/ Address not supported \n")
+        exit()
+
+    try:
+        sock.connect((addr, PORT))
+    except:
+        print("\nConnection Failed \n")
+        exit()
+
+    try:
+        _thread.start_new_thread(client_handler)
+    except:
+        print("Create thread error!\n")
+
+    while(True):
+        buffer = sock.recv(1024).decode()
+        if(len(buffer) != 0):
+            print("%s\n" % buffer)
+        if(stop == 1):
+            break
+
+
+if __name__=="__main__":
+    main(sys.argv[1])
+    
+
